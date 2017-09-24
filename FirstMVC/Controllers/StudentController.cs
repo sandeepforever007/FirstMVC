8398651ef0d8fdecc6d76ab25015fd5277e818a4
sandeepforever007 <snalla@uncc.edu>
2017-09-23 22:47:10 -0500

    Initial commit

---
+++ FirstMVC/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVC.Models;
+
+
+namespace FirstMVC.Controllers
+{
+	public class StudentController : Controller
+	{
+		// GET: Student
+
+		public ActionResult Index(int id)
+		{
+			
+			{
+				
+
+				using (UniversityEntities edb = new UniversityEntities())
+				{
+					var emp = edb.tbl_Student.ToList().SingleOrDefault(x => x.ID == id);
+					return View(emp);
+				}
+			}
+
+
+
+		}
+	}
+}
---
+++ FirstMVC/CustomFilterCSVFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC
+{
+	public class CustomFilterCSVFile : ActionFilterAttribute
+	{
+
+
+
+		public HttpRequest UsrAgnt
+		{
+			get
+			{
+				return HttpContext.Current.ApplicationInstance.Request;
+			}
+		}
+
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			string a = UsrAgnt.UserAgent;
+			string b = DateTime.Now.ToString();
+			string strUsrAgnt = $"{a},{b}";
+
+			LogData(strUsrAgnt);
+
+
+		}
+
+		public void LogData(string data)
+		{
+			File.AppendAllText(HttpContext.Current.Server.MapPath("~/Data/Data.csv"), data);
+		}
+
+
+
+
+	}
+}
